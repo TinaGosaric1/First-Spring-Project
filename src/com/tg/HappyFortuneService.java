4daa3752038a740340775f8cca487c9bc5b0b12b
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-21 09:26:16 +0100

    Injecting values from the Properties File

--- src/com/tg/HappyFortuneService.java
+++ src/com/tg/HappyFortuneService.java
@@ -1,8 +1,22 @@
 package com.tg;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
+
+    private ArrayList<String> fortunes = new ArrayList<>();
+    private Random myRandom = new Random();
+
+    public HappyFortuneService() {
+        this.fortunes.add("My First Fortune");
+        this.fortunes.add("My Second Fortune");
+        this.fortunes.add("My Third Fortune");
+    }
+
     @Override
     public String getFortune() {
-        return "Today is your lucky day :)";
+        int index = myRandom.nextInt(fortunes.size());
+        return fortunes.get(index);
     }
 }
--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -9,20 +9,13 @@ public class Main {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // retrieve bean from spring container
-        BaseballCoach theBCoach = context.getBean("myBaseballCoach", BaseballCoach.class);
-        TrackCoach theTCoach = context.getBean("myTrackCoach", TrackCoach.class);
         FootballCoach theFCoach = context.getBean("myFootballCoach", FootballCoach.class);
 
         // call methods on the bean
-        System.out.println(theBCoach.getDailyWorkout());
-        System.out.println(theFCoach.getDailyWorkout());
 
-        System.out.println(theBCoach.getDailyFortune());
-        System.out.println(theFCoach.getDailyFortune());
-
-        // call our new methods to get the literal values
         System.out.println(theFCoach.getEmailAddress());
         System.out.println(theFCoach.getTeam());
+        System.out.println(theFCoach.getDailyFortune());
 
         // close the context
         context.close();
