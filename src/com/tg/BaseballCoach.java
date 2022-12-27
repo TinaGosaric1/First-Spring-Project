ff0f5dd13584d15d556ba0282b3f461b135a65bc
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-27 18:32:50 +0100

    Inject the properties file using Java annotations

--- src/com/tg/BaseballCoach.java
+++ src/com/tg/BaseballCoach.java
@@ -2,21 +2,29 @@ package com.tg;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component // now we use default bean id, in this case baseballCoach (lower first letter)
 public class BaseballCoach implements Coach {
-    private FortuneService fortuneService;
 
-    public BaseballCoach() {
-        System.out.println(">>> BaseballCoach: inside the default constructor");
-    }
+    // inject the values from properties file
+
+    @Value("${myVar.email}")
+    private String email;
+
+    @Value("${myVar.team}")
+    private String team;
+
+    private FortuneService fortuneService;
 
     // qualifier + setter injection
     @Autowired
     @Qualifier("randomFortuneService")
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
+        System.out.println("Team name: " + team);
+        System.out.println("Email: " + email);
     }
 
     @Override
