1d3c5161cf9bec5ea2a7f28e48b483fa4656e6bf
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-28 18:43:59 +0100

    Configuration + ComponentScan (no xml)

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -1,10 +1,10 @@
 package com.tg;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfiguration.class);
 
         Coach theCoach = context.getBean("tennisCoach", Coach.class);
         System.out.println(theCoach.getDailyWorkout());
---
+++ src/com/tg/SportConfiguration.java
@@ -0,0 +1,9 @@
+package com.tg;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.tg")
+public class SportConfiguration {
+}
